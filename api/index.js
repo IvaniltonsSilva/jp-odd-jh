cdedbe6641bbd2ebbbeb99df729b87d026351835
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-28 10:09:07 -0300

    chore: update shareCode to 8ONE7RBC812

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=I23DIZWKNC9&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=8ONE7RBC812&afp=oa' });
     res.end();
   }
