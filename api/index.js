153830e3549a18ad16b452509b72a8014c6db670
JCPrime01 <vanzokiller@gmail.com>
2026-06-01 10:18:28 -0300

    chore: shareCode KEQB9WIDP8O -> 3K5AUXTVN2J (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=KEQB9WIDP8O&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=3K5AUXTVN2J&afp=oa' });
     res.end();
   }
